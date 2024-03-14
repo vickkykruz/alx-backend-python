b67a5b648ab089b99abd801c4062faa49c95e2a2
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-14 04:24:47 -0700

    Made changes

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 """ This is a module that concate two string """
 
 
 def concat(str1: str, str2: str) -> str:
+    """ This return the concatanated string of str1 and str2 """
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 This is a module that pass the argument floor and return value floor
 """
@@ -7,4 +8,5 @@ import math
 
 
 def floor(n: float) -> int:
+    """ This return the vaue n as a floor """
     return math.floor(n)

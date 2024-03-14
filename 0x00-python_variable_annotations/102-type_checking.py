304449d92ab72c9d739779b7be39e09052c8f3e0
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-14 05:42:21 -0700

    Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" This is module that does the type checkng """
+
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: List, factor: int = 2) -> List:
+    """ This return the zoom_in list""" 
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
